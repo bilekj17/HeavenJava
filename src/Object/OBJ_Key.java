d600d8ee51576ad71d2084e4419b3df8c7d00915
honza <bilekj@gasos-ro.cz>
2025-10-15 19:06:34 +0200

    its working

--- src/Object/OBJ_Key.java
+++ src/Object/OBJ_Key.java
@@ -7,7 +7,7 @@ public class OBJ_Key extends SuperObject{
     public OBJ_Key(){
         name = "Key";
         try{
-            image = ImageIO.read(getClass().getResourceAsStream("/objects/key.png"));
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/key1.png"));
         }catch(IOException e){
             e.printStackTrace();
         }
